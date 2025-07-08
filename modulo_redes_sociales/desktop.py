923001c9c6f0b0d476fb606ded32462f77834bb6
Martin Zapana <martin.zapana.b@uni.pe>
2025-07-08 02:38:48 -0500

    prueba hola

--- modulo_redes_sociales/desktop.py
+++ modulo_redes_sociales/desktop.py
@@ -1,3 +1,5 @@
 # Este archivo no es necesario para la visualización del módulo en el escritorio de ERPNext.
 # La configuración correcta está en config/modulo_redes_sociales.py
 # prueba
+# holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+#holaaaa
